e311e0102f2b95d1b6e5b556876a205112a3f9b7
SSCucumber <51746257+SSCucumber@users.noreply.github.com>
2019-06-13 18:04:43 +1000

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,63 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+colorize: true
+});
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+token: auth.token,
+autorun: true
+});
+bot.on('ready', function (evt) {
+logger.info('Connected');
+logger.info('Logged in as: ');
+logger.info(bot.username + ' – (' + bot.id + ')');
+});
+
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+// Our bot needs to know if it will execute a command
+// It will listen for messages that will start with `!`
+if (message.substring(0, 1) == '!') {
+var args = message.substring(1).split(' ');
+var cmd = args[0];
+args = args.splice(1);
+switch(cmd) {
+// !ping
+case 'soup':
+bot.sendMessage({
+to: channelID,
+message: 'Heres your soup.'
+});
+break;
+
+case 'nou':
+bot.sendMessage({
+to: channelID,
+message: 'NO U'
+});
+break;
+
+case 'hello':
+bot.sendMessage({
+to: channelID,
+message: 'Hello'
+});
+break;
+
+case 'version':
+bot.sendMessage({
+to: channelID,
+message: 'Version 1.0.0 Copyright Soup Studios 2019 Ltd.'
+});
+break;
+
+
+// Just add any case commands if you want to..
+}
+}
+});
